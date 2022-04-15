5a906d984025fbc889258f2aa33ce9d0c34b0486
Yaroslav Korolev <yarehack@Yaroslavs-MacBook-Pro.local>
2022-04-15 21:42:22 +0300

    add the search by authors

--- t.py
+++ t.py
@@ -3,6 +3,8 @@ from collections import Counter
 import json
 import webbrowser
 
+
+
 with open('data.csv') as File:
 	reader = csv.reader(File)
 
@@ -18,6 +20,67 @@ with open('data.csv') as File:
 		i = i + 1
 
 
+def usage():
+	global f, q, items, t
+
+	j = 1
+	items = x[0]
+	t = x[j]
+	f = []
+	f.insert(0, 'This is too much number')
+	while j < len(x):
+		f.append(dict(zip(items, x[j])))
+		j = j + 1
+	
+	while b:
+		p = int(input('Enter the number of videos from 0 to 5440: '))
+		if p > 5440:
+			print(f[0])
+			break
+			
+		print(f[p])
+		q = f[p]
+			
+		print(q['link'])
+		ans = str(input('Do you want to watch video? (yes or no): --> '))
+		if ans == 'yes':
+			webbrowser.open(q['link'], new=0, autoraise=True)
+			break
+		else:
+			print('Okay, video is closed')
+			break
+def search():
+	p = print(f'Search by authors\n')
+	u = str(input('Name of author: '))
+	w = 1
+	while w < len(f):
+
+		if u == f[w].get('author'):
+			print('Found!')
+			res = f[w]
+			print(f[w])
+			opn = input('Do you want open this video? (yes or no): ')
+			if opn == 'yes':
+				webbrowser.open(res['link'], new=0, autoraise=True)
+			else:
+				print('Okay, video is closed')
+			break
+		else:
+			w = w + 1	
+ask = input('How do you want search videos?\n 1)Author\n 2)Data Base\n Enter a number --> ')
+if ask == '1':
+	b = False
+	usage()
+	search()
+else:
+	b = True
+	usage()
+
+
+
+
+
+
 # TO DO
 def categories():
 	pass
@@ -31,50 +94,7 @@ def categories():
 #         	continue
 
 
-p = int(input('Enter the number of videos from 0 to 5440: '))
 
 
 
 
-def usage(index):
-	global f, q
-
-	j = 1
-	prices = x[0]
-	t = x[j]
-	f = []
-	f.insert(0, 'This is too much number')
-	while j < len(x):
-		f.append(dict(zip(prices, x[j])))
-		j = j + 1
-	while True:
-
-		if p > 5440:
-			print(f[0])
-			break
-		
-		print(f[index])
-		q = f[index]
-		print(q['link'])
-		ans = str(input('Do you want to watch video? (yes or no): --> '))
-		if ans == 'yes':
-			webbrowser.open(q['link'], new=0, autoraise=True)
-			break
-		else:
-			print('Okay, video is closed')
-			break
-usage(p)
-
-
-t = x[0]
-def search():
-	p = print(f'Search by {t}\n')
-	print("1 - title\n2 - author\n3 - date")
-	u = int(input('Item: '))
-	if u == 1:
-		tit = str(input('Name of title: '))
-	if u == 2:
-		a = str(input("Name of author: "))
-		print(q['author'])
-	if u == 3:
-		dat = int(input("Date: "))
